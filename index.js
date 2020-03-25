bc86cfd0d9f7262efab117468fa02ff505a4168c
Eshi44 <e.hameliervolino@gmail.com>
2020-03-25 19:50:13 -0400

    added inquirer prompt with two types/messages(project title and project description. Also added the .then promise

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+//inquirer module from npm 
 var inquirer = require("inquirer");
+//js file system module- allows you to work with the fs on your comp
 var fs = require("fs");
+
+inquirer.prompt([
+{
+    type: "input", 
+    message: "What is your project title?", 
+    name: "projectTitle",
+},
+{
+    type: "input", 
+    message: "What is your project's description?", 
+    name: "projectDescription"
+}
+
+]).then(function(resp) {
+    console.log("Your project title is " + resp.projectTitle);
+
+});
